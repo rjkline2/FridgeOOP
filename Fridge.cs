211735791aba0431b5f1977ddcea31b27e06da11
RJ Kline <rj.kline@gmail.com>
2018-01-31 14:08:54 -0500

    FidgeOOP initial upload

---
+++ Fridge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeOOP
+{
+   public class Fridge
+    {
+        //fields 
+        private bool isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+        //Properties
+        public bool isClean
+        {
+            get { return.this.isClean; }
+            set { this.isClean = value;  }
+        }
+
+        public string DispenserType
+        {
+            get { return this.dispenserType; }
+        }
+        //Constructors
+
+
+        //Methods()
+
+
+    }
+
+}
